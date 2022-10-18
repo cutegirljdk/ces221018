cde026a59c13bbe9aefc4cbb1aa66f3ca2046b94
cutegirljdk <chaeeunseoces@gmai.com>
2022-10-18 16:55:22 +0900

    iu member branch 에서 MemberVo id, register

--- webstudy88-ces1018/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy88-ces1018/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -4,5 +4,19 @@ import java.io.Serializable;
 
 public class MemberVO implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	String id;
+	public MemberVO(String id) {
+		super();
+		this.id = id;
+	}
+	public MemberVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
 }
